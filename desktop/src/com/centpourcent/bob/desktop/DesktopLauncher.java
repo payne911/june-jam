56a230c71e577128844cd8ee80dcdef445eaa861
payne911 <payne911@users.noreply.github.com>
2020-06-04 22:16:07 -0400

    Initial commit

---
+++ desktop/src/com/centpourcent/bob/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.centpourcent.bob.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.centpourcent.bob.BobGoestoTheBeach;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BobGoestoTheBeach(), config);
+	}
+}
