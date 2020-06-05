38e307be2878a8ac435838494a8ab6fedd7a6056
payne911 <payne911@users.noreply.github.com>
2020-06-04 22:24:20 -0400

    Renamed main class

--- desktop/src/com/centpourcent/bob/desktop/DesktopLauncher.java
+++ desktop/src/com/centpourcent/bob/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.centpourcent.bob.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.centpourcent.bob.BobGoestoTheBeach;
+import com.centpourcent.bob.BobGoesToTheBeach;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BobGoestoTheBeach(), config);
+		new LwjglApplication(new BobGoesToTheBeach(), config);
 	}
 }
