9027418fcc229937cb5af509082236b20fb9aa46
payne911 <payne911@users.noreply.github.com>
2020-06-10 22:24:49 -0400

    Forgot actual register

--- core/src/com/marvelousbob/client/network/register/Register.java
+++ core/src/com/marvelousbob/client/network/register/Register.java
@@ -8,5 +8,9 @@ public class Register {
     public static void registerClasses(EndPoint registrar) {
         registrar.getKryo().register(Msg.class);
         registrar.getKryo().register(Ping.class);
+        registrar.getKryo().register(GameState.class);
+        registrar.getKryo().register(MoveAction.class);
+        registrar.getKryo().register(TestClass.class);
+        registrar.getKryo().register(TestClass2.class);
     }
 }
